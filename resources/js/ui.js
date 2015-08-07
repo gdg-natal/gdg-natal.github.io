8de49bba4b0d81d008af53b94a73e629dc1bcbc6
jeffersonmourak <jefinho.moura14@gmail.com>
2015-08-07 18:27:22 -0300

    New Design

---
+++ resources/js/ui.js
@@ -0,0 +1,98 @@
+(function() {
+    "use strict";
+
+    function Animation() {
+        this.animations = {
+            bounce: "anim-bounce",
+            ease: "anim-ease",
+            none: "anim-none"
+        };
+
+        this.timeLine = {};
+    }
+
+    Animation.prototype = {
+        resetAnimations: function(selector) {
+            var classes = $(selector).attr("class").split(" ");
+            for (var i in classes) {
+                var _class = classes[i];
+                if (_class.indexOf("anim") > -1) {
+                    $(selector).removeClass(_class);
+                }
+            }
+        },
+        set: function(selector, animation) {
+            this.resetAnimations(selector);
+            $(selector).addClass(this.animations[animation]);
+        },
+        animate: function() {
+            var time = 0;
+            var self = this;
+            setInterval(function() {
+                if (self.timeLine[time] !== undefined) {
+                    self.timeLine[time]();
+                }
+                time++;
+            }, 1);
+        }
+    }
+
+    var animation = new Animation();
+
+    $(document).ready(function() {
+
+        $('html, body').css({
+            'overflow': 'hidden',
+            'height': '100%'
+        });
+
+        animation.timeLine = {
+            300: function() {
+                animation.set(".logo", "bounce");
+                $(".logo").removeClass("closed");
+            },
+            500: function() {
+                animation.set(".logo, .nav, .title", "ease");
+                $(".logo").addClass("up");
+            },
+            1000: function() {
+                $(".header").removeClass("openend");
+                $(".header").addClass("closed");
+                $(".nav").addClass("opened");
+                $(".nav").removeClass("closed");
+                $(".header").css("position", "absolute");
+                $(".title").css("opacity", "0");
+                $('html, body').css({
+                    'overflow': 'auto',
+                    'height': 'auto'
+                });
+            },
+            1200: function() {
+                $(".logo").css("position", "fixed");
+                $(".nav").removeClass("opened");
+                $(".title").css("display", "none");
+            }
+
+        }
+
+        animation.animate();
+
+        $("#initiatives").owlCarousel({
+            singleItem: true,
+        });
+
+        $(".navigate").click(function(e){
+        	e.preventDefault();
+
+        	var target = $(this).attr("href");
+        	
+        	var scrollPosition = $(target).offset().top;
+
+        	$('body').animate({scrollTop:scrollPosition},300);
+
+
+        });
+
+    });
+
+}());
